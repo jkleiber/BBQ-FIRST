7dcd8bf5c5c585ba08bf81215a90c3a03fad1193
Justin Kleiber <jkleiber8@gmail.com>
2024-03-20 21:57:11 -0600

    Improves theming and modularity

--- src/components/MainPageHeader.vue
+++ src/components/MainPageHeader.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink } from 'vue-router';
-
 </script>
 
 <template>
@@ -27,4 +26,56 @@ import { RouterLink } from 'vue-router';
 export default {}
 </script>
 
-<style scoped></style>
+<style scoped>
+a.nav {
+    background-image: url("/assets/navicon.png");
+}
+
+a.nav_txt {
+    transition: color .5s ease;
+    transition: background-color .5s ease;
+    -ms-transition: color .5s ease;
+    -ms-transition: background-color .5s ease;
+    -moz-transition: color .5s ease;
+    -moz-transition: background-color .5s ease;
+    -webkit-transition: color .5s ease;
+    -webkit-transition: background-color .5s ease;
+}
+
+
+a.nav_txt:link,
+a.nav_txt:visited {
+
+    -webkit-transition: border .5s ease;
+    -moz-transition: border .5s ease;
+    transition: border .5s ease;
+    position: relative;
+    /* background-color: #FF7A00; */
+    text-decoration: none;
+    color: #FFF;
+    margin: 0 auto;
+    float: right;
+    font-size: 16px;
+    padding: 14.5px;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    -ms-border-radius: 5px;
+    border-radius: 5px;
+    margin: 10;
+}
+
+a.nav_txt:hover,
+a.nav_txt:active {
+    background-color: --md-sys-color-primary;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    border-radius: 5px;
+    text-decoration: none;
+    margin: 0 auto;
+    color: #FFF;
+    float: right;
+    font-size: 16px;
+    padding: 14.5px;
+    margin: 10;
+}
+</style>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,11 +1,11 @@
 <script setup>
 import MainPageHeader from '@/components/MainPageHeader.vue';
 
-import '@/assets/bbq.css';
-
 import '@material/web/tabs/tabs';
 import '@material/web/tabs/primary-tab';
 import '@material/web/icon/icon';
+import '@material/web/textfield/outlined-text-field';
+import '@material/web/button/filled-button';
 
 </script>
 
@@ -13,27 +13,30 @@ import '@material/web/icon/icon';
     <MainPageHeader></MainPageHeader>
     <div class="main-content">
         <form>
-            <input type="number" name="teamNumber" v-model="teamNumber">
-            <button type="submit" @click.stop.prevent="submit()">Submit</button>
+            <md-outlined-text-field type="number" label="Team Number" v-model="teamNumber"
+                @keydown.enter="submit()"></md-outlined-text-field>
+            <md-filled-button class="v-centered-button" type="submit"
+                @click.stop.prevent="submit()">Submit</md-filled-button>
         </form>
 
         <!-- Show team information if it exists -->
-        <h1> {{ teamTitleText }} </h1>
+        <div v-if="doesTeamExist()">
+            <h1> {{ teamTitleText }} </h1>
 
+            <md-tabs>
+                <md-primary-tab v-for="tab in tabs" @click="changeTab(tab)" :key="tab.name">
+                    <md-icon slot="icon">{{ tab.icon }}</md-icon>
+                    {{ tab.name }}
+                </md-primary-tab>
+            </md-tabs>
 
-        <md-tabs>
-            <md-primary-tab v-for="tab in tabs" @click="changeTab(tab)" :key="tab.name">
-                <md-icon slot="icon">{{ tab.icon }}</md-icon>
-                {{ tab.name }}
-            </md-primary-tab>
-        </md-tabs>
+            <div id="robot-panel" role="tabpanel" aria-labelledby="robot-tab" v-if="isActive(0)">
+                Robot Awards
+            </div>
 
-        <div id="robot-panel" role="tabpanel" aria-labelledby="robot-tab" v-if="isActive(0)">
-            Robot Awards
-        </div>
-
-        <div id="team-panel" role="tabpanel" aria-labelledby="team-tab" v-if="isActive(1)">
-            Team Awards
+            <div id="team-panel" role="tabpanel" aria-labelledby="team-tab" v-if="isActive(1)">
+                Team Awards
+            </div>
         </div>
     </div>
 </template>
@@ -44,7 +47,7 @@ export default {
     data() {
         return {
             teamNumber: this.$route.params.team_number,
-            teamString: null,
+            teamString: "",
             tabs: [
                 { id: 0, name: "Robot Performance", icon: "smart_toy" },
                 { id: 1, name: "Team Attribute", icon: "diversity_3" }
@@ -85,6 +88,9 @@ export default {
         },
         isActive(id) {
             return id == this.activeTab;
+        },
+        doesTeamExist() {
+            return this.$route.params.team_number != null && this.$route.params.team_number > 0;
         }
     },
 }
