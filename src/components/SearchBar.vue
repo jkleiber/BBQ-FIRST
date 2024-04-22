d6bd9a1ee25e8ea00c2dfc634f1f99bcc34769e8
Justin Kleiber <jkleiber8@gmail.com>
2024-04-21 23:50:14 -0400

    Begins working on improving search

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,68 @@
+<script setup>
+
+import '@material/web/textfield/outlined-text-field';
+import '@material/web/button/filled-button';
+
+</script>
+
+
+<template>
+    <form class="search-form">
+        <md-outlined-text-field class="search-text-field" type="text" v-bind:label="inputLabelText" v-model="searchVal"
+            @keydown.enter="emitSubmit()"></md-outlined-text-field>
+    </form>
+</template>
+
+<script>
+export default {
+    props: ['label', 'searchData'],
+    data() {
+        return {}
+    },
+    computed: {
+        inputLabelText() {
+            return this.label;
+        },
+        inputType() {
+            return this.type;
+        },
+        searchVal: {
+            get() {
+                return this.modelValue;
+            },
+            set(val) {
+                this.$emit('update:modelValue', val);
+            }
+        }
+    },
+    methods: {
+        emitSubmit() {
+            this.$emit('submit');
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+form.search-form {
+    display: flex;
+    height: 100px;
+}
+
+.search-v-centered-button {
+    top: 50%;
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
+    height: fit-content;
+}
+
+md-outlined-text-field {
+    resize: none;
+    margin: 10px;
+}
+
+.search-text-field {
+    align-items: center;
+}
+</style>
